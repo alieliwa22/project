d2dff2e85257c4f411c21ffa6f8857dd780d82cb
Ali Eliwa <56533995+alieliwa22@users.noreply.github.com>
2021-10-13 00:36:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const loginBtn = document.getElementById("login-btn");
+// const registerBtn = document.getElementById("register-btn");
+// const loginSection = document.getElementById("login-section");
+// const registerSection = document.getElementById("register-section");
+// const leftSection = document.getElementById("left-section");
+// loginBtn.addEventListener("click", () => {
+//   loginSection.classList.remove("displayn");
+//   registerSection.classList.add("displayn");
+//   loginBtn.classList.add("active");
+//   registerBtn.classList.remove("active");
+// });
+// registerBtn.addEventListener("click", () => {
+//   loginSection.classList.add("displayn");
+//   registerSection.classList.remove("displayn");
+//   loginBtn.classList.remove("active");
+//   registerBtn.classList.add("active");
+// });
+
+
